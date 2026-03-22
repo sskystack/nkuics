f3bc72c7862a98dab80f944848564a07e26be9b1
tracer-ics2017 <tracer@njuics.org>
2026-03-22 15:10:41 +0800

    >  compile 2311082 alice Linux alice-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  15:10:41 up 30 min,  1 user,  load average: 0.00, 0.01, 0.00 1d572a4d1dde2c09837b40aa91d47485d73abefe

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -80,4 +80,29 @@ void print_wp() {
   }
 }
 
+bool check_watchpoints() {
+  WP *wp = head;
+  while (wp != NULL) {
+    bool success = false;
+    uint32_t new_val = expr(wp->expr, &success);
+    if (!success) {
+      printf("Failed to evaluate watchpoint %d: %s\n", wp->NO, wp->expr);
+      wp = wp->next;
+      continue;
+    }
+
+    if (new_val != wp->old_val) {
+      printf("Watchpoint %d triggered: %s\n", wp->NO, wp->expr);
+      printf("Old value = %u\n", wp->old_val);
+      printf("New value = %u\n", new_val);
+      wp->old_val = new_val;
+      return true;
+    }
+
+    wp = wp->next;
+  }
+
+  return false;
+}
+
 
