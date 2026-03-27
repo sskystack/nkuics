5b69129bedfa0b2b33c25e8e0fa0fef9f530dc9b
tracer-ics2017 <tracer@njuics.org>
2026-03-28 00:12:36 +0800

    >  compile 2311082 alice Linux alice-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  00:12:36 up 36 min,  1 user,  load average: 0.00, 0.00, 0.00 6d3a7a2ac29c06495150826eb759d33119809021

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -126,17 +126,22 @@ make_rtl_setget_eflags(SF)
 
 static inline void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1) {
   // dest <- src1
-  TODO();
+  *dest = *src1;
 }
 
 static inline void rtl_not(rtlreg_t* dest) {
   // dest <- ~dest
-  TODO();
+  *dest = ~(*dest);
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  switch (width) {
+    case 1: *dest = (int32_t)(int8_t)(*src1); return;
+    case 2: *dest = (int32_t)(int16_t)(*src1); return;
+    case 4: *dest = (int32_t)(*src1); return;
+    default: assert(0);
+  }
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
@@ -155,12 +160,12 @@ static inline void rtl_pop(rtlreg_t* dest) {
 
 static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 == 0 ? 1 : 0)
-  TODO();
+  *dest = (*src1 == 0);
 }
 
 static inline void rtl_eqi(rtlreg_t* dest, const rtlreg_t* src1, int imm) {
   // dest <- (src1 == imm ? 1 : 0)
-  TODO();
+  *dest = (*src1 == (rtlreg_t)imm);
 }
 
 static inline void rtl_neq0(rtlreg_t* dest, const rtlreg_t* src1) {
