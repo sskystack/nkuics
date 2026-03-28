3c9bc0c61b4c93823f7892c73191a920bb4d69b2
tracer-ics2017 <tracer@njuics.org>
2026-03-28 10:43:38 +0800

    >  compile 2311082 alice Linux alice-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  10:43:37 up 11:07,  1 user,  load average: 0.08, 0.02, 0.01 4745107b556c343237bf6a2676385e71226b231

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -71,7 +71,22 @@ make_EHelper(cmp) {
 }
 
 make_EHelper(inc) {
-  TODO();
+  rtl_li(&t1, 1);
+  rtl_add(&t2, &id_dest->val, &t1);
+  if (id_dest->width != 4) {
+    rtl_andi(&t2, &t2, id_dest->width == 1 ? 0xff : 0xffff);
+  }
+  operand_write(id_dest, &t2);
+
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  // OF for inc: same as add with src = 1
+  rtl_xor(&t0, &id_dest->val, &t1);
+  rtl_not(&t0);
+  rtl_xor(&t3, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t3);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
 
   print_asm_template1(inc);
 }
