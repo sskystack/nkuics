1f6e70f45d03db306294a6064ec761e8be4f21be
2311082-周重天 <2311082@mail.nankai.edu.cn>
2026-04-04 20:35:02 +0800

    interrupt

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,9 +5,28 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
+  rtlreg_t tmp;
+  uint32_t idt_addr = cpu.idtr.base + NO * 8;
+  uint32_t low = vaddr_read(idt_addr, 4);
+  uint32_t high = vaddr_read(idt_addr + 4, 4);
+  uint32_t handler = (low & 0x0000ffff) | (high & 0xffff0000);
 
-  TODO();
+  tmp = cpu.eflags;
+  rtl_push(&tmp);
+
+  tmp = 0;
+  rtl_push(&tmp);
+
+  tmp = ret_addr;
+  rtl_push(&tmp);
+
+  cpu.eip = handler;
 }
 
 void dev_raise_intr() {
+  raise_intr(0x81, cpu.eip);
+}
+
+void dev_raise_io_intr() {
+  raise_intr(0x82, cpu.eip);
 }
