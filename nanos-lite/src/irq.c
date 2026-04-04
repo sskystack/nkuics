e356bb89816c07d9c011c3c3013b2851e9320864
2311082-周重天 <2311082@mail.nankai.edu.cn>
2026-04-04 21:09:57 +0800

    dispatch

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,11 +1,14 @@
 #include "common.h"
 
+_RegSet* do_syscall(_RegSet *r);
+
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
+    case _EVENT_SYSCALL: return do_syscall(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
 
-  return NULL;
+  return r;
 }
 
 void init_irq(void) {
