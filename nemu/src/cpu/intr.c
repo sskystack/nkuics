f2e71de850ecaf91f8d0a4ea6e1ed86b18152ff1
2311082-周重天 <2311082@mail.nankai.edu.cn>
2026-04-04 21:55:42 +0800

    hello

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -10,6 +10,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   uint32_t low = vaddr_read(idt_addr, 4);
   uint32_t high = vaddr_read(idt_addr + 4, 4);
   uint32_t handler = (low & 0x0000ffff) | (high & 0xffff0000);
+  uint32_t gate_type = (high >> 8) & 0xf;
 
   tmp = cpu.eflags;
   rtl_push(&tmp);
@@ -19,6 +20,11 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
 
   tmp = ret_addr;
   rtl_push(&tmp);
+
+  // interrupt gate clears IF, trap gate keeps IF unchanged
+  if (gate_type == 0xe) {
+    cpu.IF = 0;
+  }
 
   cpu.eip = handler;
 }
