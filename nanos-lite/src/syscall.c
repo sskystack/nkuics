145f042e4e8f7c5ad496f3f35a33ce47cb88c56d
2311082-周重天 <2311082@mail.nankai.edu.cn>
2026-04-04 21:14:01 +0800

    syscall

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,10 +4,19 @@
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
+    case SYS_none:
+      SYSCALL_ARG1(r) = 1;
+      break;
+    case SYS_exit:
+      _halt((int)a[1]);
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
-  return NULL;
+  return r;
 }
