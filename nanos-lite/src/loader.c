da96868ea01f7cb7f9b6b71091baee975f67c3df
tracer-ics2017 <tracer@njuics.org>
2026-04-04 14:08:20 +0800

    >  compile 2311082 alice Linux alice-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  14:08:20 up  1:24,  1 user,  load average: 0.01, 0.03, 0.00 714c4d9958aa583f339e6c20eeb4f1424c55a9b2

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,16 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+void ramdisk_read(void *buf, off_t offset, size_t len);
+size_t get_ramdisk_size();
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  // TODO();
+  (void)as;
+  (void)filename;
+
+  size_t img_size = get_ramdisk_size();
+  ramdisk_read(DEFAULT_ENTRY, 0, img_size);
+
   return (uintptr_t)DEFAULT_ENTRY;
 }
