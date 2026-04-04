b286dbe02ead84322d46705c9ecefee4ec9870eb
tracer-ics2017 <tracer@njuics.org>
2026-04-04 09:37:02 -0700

    >  run 2311082 alice Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  09:37:01 up 31 min,  1 user,  load average: 0.28, 0.12, 0.04 a22b065e83f3bd3bffdc33b1209c772c9779e6f9

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -8,16 +8,46 @@ static const char *keyname[256] __attribute__((used)) = {
   _KEYS(NAME)
 };
 
+#define KEYDOWN_MASK 0x8000
+
 size_t events_read(void *buf, size_t len) {
-  return 0;
+  int key = _read_key();
+  if (key == _KEY_NONE) {
+    return 0;
+  }
+
+  bool is_keydown = (key & KEYDOWN_MASK) != 0;
+  key &= ~KEYDOWN_MASK;
+
+  int n = snprintf((char *)buf, len, "%s %s\n", is_keydown ? "kd" : "ku", keyname[key]);
+  if (n < 0) return 0;
+  return n;
 }
 
 static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
+  memcpy(buf, dispinfo + offset, len);
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
+  const uint32_t *pixels = (const uint32_t *)buf;
+  int width = _screen.width;
+  int pixel_off = offset / sizeof(uint32_t);
+  int x = pixel_off % width;
+  int y = pixel_off / width;
+  int n = len / sizeof(uint32_t);
+
+  while (n > 0) {
+    int w = width - x;
+    if (w > n) w = n;
+    _draw_rect(pixels, x, y, w, 1);
+    pixels += w;
+    n -= w;
+    x = 0;
+    y++;
+  }
+  _draw_sync();
 }
 
 void init_device() {
@@ -25,4 +55,5 @@ void init_device() {
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
+  snprintf(dispinfo, sizeof(dispinfo), "WIDTH:%d\nHEIGHT:%d\n", _screen.width, _screen.height);
 }
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -4,6 +4,7 @@ typedef struct {
   char *name;
   size_t size;
   off_t disk_offset;
+  off_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
@@ -21,6 +22,101 @@ static Finfo file_table[] __attribute__((used)) = {
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 
+void ramdisk_read(void *buf, off_t offset, size_t len);
+void ramdisk_write(const void *buf, off_t offset, size_t len);
+size_t events_read(void *buf, size_t len);
+void dispinfo_read(void *buf, off_t offset, size_t len);
+void fb_write(const void *buf, off_t offset, size_t len);
+
+int fs_open(const char *pathname, int flags, int mode) {
+  (void)flags;
+  (void)mode;
+  for (int i = 0; i < NR_FILES; i++) {
+    if (strcmp(file_table[i].name, pathname) == 0) {
+      file_table[i].open_offset = 0;
+      return i;
+    }
+  }
+  panic("file not found: %s", pathname);
+  return -1;
+}
+
+size_t fs_read(int fd, void *buf, size_t len) {
+  assert(fd >= 0 && fd < NR_FILES);
+
+  if (fd == FD_STDIN) return 0;
+  if (fd == FD_EVENTS) return events_read(buf, len);
+
+  Finfo *f = &file_table[fd];
+  if (f->open_offset >= (off_t)f->size) return 0;
+  if (f->open_offset + len > f->size) {
+    len = f->size - f->open_offset;
+  }
+
+  if (fd == FD_DISPINFO) {
+    dispinfo_read(buf, f->open_offset, len);
+  }
+  else {
+    ramdisk_read(buf, f->disk_offset + f->open_offset, len);
+  }
+  f->open_offset += len;
+  return len;
+}
+
+size_t fs_write(int fd, const void *buf, size_t len) {
+  assert(fd >= 0 && fd < NR_FILES);
+
+  if (fd == FD_STDOUT || fd == FD_STDERR) {
+    const char *p = (const char *)buf;
+    for (size_t i = 0; i < len; i++) {
+      _putc(p[i]);
+    }
+    return len;
+  }
+
+  Finfo *f = &file_table[fd];
+  if (fd == FD_FB) {
+    fb_write(buf, f->open_offset, len);
+    f->open_offset += len;
+    return len;
+  }
+
+  if (f->open_offset >= (off_t)f->size) return 0;
+  if (f->open_offset + len > f->size) {
+    len = f->size - f->open_offset;
+  }
+  ramdisk_write(buf, f->disk_offset + f->open_offset, len);
+  f->open_offset += len;
+  return len;
+}
+
+off_t fs_lseek(int fd, off_t offset, int whence) {
+  assert(fd >= 0 && fd < NR_FILES);
+  Finfo *f = &file_table[fd];
+
+  off_t new_offset = 0;
+  switch (whence) {
+    case SEEK_SET: new_offset = offset; break;
+    case SEEK_CUR: new_offset = f->open_offset + offset; break;
+    case SEEK_END: new_offset = f->size + offset; break;
+    default: panic("bad whence = %d", whence);
+  }
+
+  assert(new_offset >= 0 && new_offset <= (off_t)f->size);
+  f->open_offset = new_offset;
+  return f->open_offset;
+}
+
+int fs_close(int fd) {
+  assert(fd >= 0 && fd < NR_FILES);
+  return 0;
+}
+
+size_t fs_filesz(int fd) {
+  assert(fd >= 0 && fd < NR_FILES);
+  return file_table[fd].size;
+}
+
 void init_fs() {
-  // TODO: initialize the size of /dev/fb
+  file_table[FD_FB].size = _screen.width * _screen.height * sizeof(uint32_t);
 }
