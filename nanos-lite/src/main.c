a45d560b6e23b0832fc6f321dcd98b5b032e5194
tracer-ics2017 <tracer@njuics.org>
2026-04-06 05:53:57 -0700

    >  run 2311082 alice Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  05:53:57 up 11 min,  1 user,  load average: 0.18, 0.24, 0.17 41b47ab61212203a703671011e3685d6bedc7abd

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -33,7 +33,7 @@ int main() {
   Log("File system initialized");
 
   Log("Loading user program: /bin/events");
-  uint32_t entry = loader(NULL, "/bin/events");
+  uint32_t entry = loader(NULL, "/bin/pal");
   Log("Jumping to entry = 0x%x", entry);
   ((void (*)(void))entry)();
 
