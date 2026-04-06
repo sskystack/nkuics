af0912cb3e6eea1cd22a92b75678bd84b9872b8f
tracer-ics2017 <tracer@njuics.org>
2026-04-05 22:13:31 -0700

    >  compile 2311082 alice Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  22:13:31 up 11 min,  1 user,  load average: 0.45, 0.47, 0.30 8b5dda5cd5ea8d517beaf4a9e46215c8a27c8b50

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -40,7 +40,7 @@ size_t fb_write(const void *buf, off_t offset, size_t len) {
   const uint32_t *pixels = (const uint32_t *)buf;
   static int fb_pixel_log_cnt = 0;
   if (fb_pixel_log_cnt < 3 && len >= sizeof(uint32_t)) {
-    Log("fb_write: offset=%d len=%d first_pixel=0x%08x", offset, len, pixels[0]);
+    Log("fb_write: offset=%d len=%d first_pixel=%x", offset, len, pixels[0]);
     fb_pixel_log_cnt++;
   }
   int width = _screen.width;
