188ddd26c1223449e0d6a75c8968d38011db1d09
tracer-ics2017 <tracer@njuics.org>
2026-05-11 06:12:34 -0700

    >  run 2311082 alice Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  06:12:34 up 10:58,  1 user,  load average: 0.28, 0.07, 0.02 763a8c663816b51d4f03c28b779d13be5a666ba3

--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -105,13 +105,12 @@ _RegSet *_umake(_Protect *p, _Area ustack, _Area kstack, void *entry, char *cons
   *(--sp) = 0;
   *(--sp) = 0;
   *(--sp) = 0;
-  *(--sp) = 0;
 
   _RegSet *tf = (_RegSet *)((uintptr_t)sp - sizeof(_RegSet));
   tf->edi = 0;
   tf->esi = 0;
   tf->ebp = 0;
-  tf->esp = (uintptr_t)sp;
+  tf->esp = 0;
   tf->ebx = 0;
   tf->edx = 0;
   tf->ecx = 0;
@@ -120,7 +119,7 @@ _RegSet *_umake(_Protect *p, _Area ustack, _Area kstack, void *entry, char *cons
   tf->error_code = 0;
 
   tf->eip = (uintptr_t)entry;
-  tf->cs = KSEL(SEG_KCODE);
+  tf->cs = 0;
   tf->eflags = FL_IF | 0x2;
 
   return tf;
