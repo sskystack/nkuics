859d7ffb23f9e251425a5d57e27f87f0f8621948
tracer-ics2017 <tracer@njuics.org>
2026-05-11 08:38:06 -0700

    >  run 2311082 alice Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  08:38:06 up 13:23,  1 user,  load average: 0.20, 0.07, 0.17 996759d52680b4e28352763e5c629f8682087ecf

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -19,6 +19,7 @@ int mm_brk(uint32_t new_brk) {
   assert(current != NULL);
 
   if (current->cur_brk == 0) {
+    Log("mm_brk init: new_brk=%p", (void *)new_brk);
     current->cur_brk = new_brk;
     current->max_brk = PGROUNDUP(new_brk);
   }
@@ -26,6 +27,10 @@ int mm_brk(uint32_t new_brk) {
     if (new_brk > current->max_brk) {
       uintptr_t map_start = PGROUNDUP(current->max_brk);
       uintptr_t map_end = PGROUNDUP(new_brk);
+
+      Log("mm_brk grow: cur=%p max=%p new=%p map=[%p, %p)",
+          (void *)current->cur_brk, (void *)current->max_brk,
+          (void *)new_brk, (void *)map_start, (void *)map_end);
 
       for (uintptr_t va = map_start; va < map_end; va += PGSIZE) {
         void *pa = new_page();
