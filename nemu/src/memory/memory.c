0034a7cbc2d19d7c2fd9f1aeabac0a8672eadda7
tracer-ics2017 <tracer@njuics.org>
2026-05-10 19:39:39 -0700

    >  compile 2311082 alice Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  19:39:39 up 25 min,  1 user,  load average: 0.00, 0.03, 0.15 55ab269b143a2202d96bb4883bdc1e00892ba932

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -12,6 +12,15 @@ uint8_t pmem[PMEM_SIZE];
 
 /* Memory accessing interfaces */
 
+#define CR0_PG_MASK 0x80000000
+#define PTE_P_MASK  0x001
+#define PTE_A_MASK  0x020
+#define PTE_D_MASK  0x040
+#define PTE_ADDR(x) ((x) & ~PAGE_MASK)
+#define PDX(addr)   (((addr) >> 22) & 0x3ff)
+#define PTX(addr)   (((addr) >> 12) & 0x3ff)
+#define OFF(addr)   ((addr) & PAGE_MASK)
+
 uint32_t paddr_read(paddr_t addr, int len) {
   int map_NO = is_mmio(addr);
   if (map_NO != -1) {
@@ -29,10 +38,49 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
   memcpy(guest_to_host(addr), &data, len);
 }
 
+static inline paddr_t page_translate(vaddr_t addr, bool is_write) {
+  if ((cpu.cr0.val & CR0_PG_MASK) == 0) {
+    return addr;
+  }
+
+  paddr_t pdir_base = PTE_ADDR(cpu.cr3.val);
+  paddr_t pde_addr = pdir_base + PDX(addr) * sizeof(PDE);
+  PDE pde;
+  pde.val = paddr_read(pde_addr, sizeof(PDE));
+  assert(pde.present);
+
+  if (!pde.accessed) {
+    pde.accessed = 1;
+    paddr_write(pde_addr, sizeof(PDE), pde.val);
+  }
+
+  paddr_t ptab_base = PTE_ADDR(pde.val);
+  paddr_t pte_addr = ptab_base + PTX(addr) * sizeof(PTE);
+  PTE pte;
+  pte.val = paddr_read(pte_addr, sizeof(PTE));
+  assert(pte.present);
+
+  pte.accessed = 1;
+  if (is_write) {
+    pte.dirty = 1;
+  }
+  paddr_write(pte_addr, sizeof(PTE), pte.val);
+
+  return PTE_ADDR(pte.val) + OFF(addr);
+}
+
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  uint32_t data = 0;
+  for (int i = 0; i < len; i ++) {
+    paddr_t paddr = page_translate(addr + i, false);
+    data |= paddr_read(paddr, 1) << (i * 8);
+  }
+  return data;
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  paddr_write(addr, len, data);
+  for (int i = 0; i < len; i ++) {
+    paddr_t paddr = page_translate(addr + i, true);
+    paddr_write(paddr, 1, (data >> (i * 8)) & 0xff);
+  }
 }
